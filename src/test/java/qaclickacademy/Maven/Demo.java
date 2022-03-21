c761bef90c02f34da2bab458b7d428230e75972d
Dhananjay <Dhananjay_9922@rediffmail.com>
2022-03-21 10:08:13 +0530

    New java class method

---
+++ src/test/java/qaclickacademy/Maven/Demo.java
@@ -0,0 +1,15 @@
+package qaclickacademy.Maven;
+
+import org.testng.annotations.Test;
+
+public class Demo 
+{
+	
+   @Test
+   public void demo()
+   {
+	   System.out.println("Demo Method");
+   }
+	
+
+}
